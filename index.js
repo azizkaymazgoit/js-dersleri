dad22cab96220d37597c9feb4cb815434fc4de0d
azizkaymaz <azizkaymaz@gmail.com>
2025-02-12 19:37:01 +0300

    ders 1

---
+++ index.js
@@ -0,0 +1,38 @@
+// let - değeri değişebilen
+// const - değeri değişmez
+
+const baslik = "merhaba";
+
+let data = "sunucudan gelen data - sınıf öğrenci listesi";
+
+data = "asdasd";
+
+
+const benBirDegiskenim = "Merhaba";
+const BenBirDegiskenim = "Selam"
+const merhaba_degisken = "asd"
+const elseDegiskenim = "asdas";
+
+// String, number, boolean, undefined, null
+
+/*const degiskenString = "asdas"; // String
+const degiskenNumber = 10; // Number
+const degiskenBoolean = false // true - false, 1 - 0
+const degisken1 = undefined;
+const degisken2 = null;
+
+let degisken000; */
+
+// + - * / %
+
+
+
+/* let sayi1 = 20;
+sayi1 = sayi1 + 20
+sayi1 += 30
+
+let siteBasligi = "Yeni Web Sitem";
+
+siteBasligi = "yeni site 2";*/
+
+console.log(22 % 7);
